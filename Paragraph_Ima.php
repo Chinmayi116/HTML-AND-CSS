c3bc6f6bcc7368fb467855af393ee7f2deba8ef5
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-07-29 21:29:44 +0530

    Add initial HTML structure in Paragraph_Ima.php

---
+++ Paragraph_Ima.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+#box{
+width:400px;
+margin:auto;
+position:relative;
+text-align:center;
+font-size:20px;
+line-height:20px;
+}
+
+img{
+position:absolute;
+top:50%;
+left:50%;
+transform:translate(-50%,-50%);
+opacity:0.3;
+width:120px;
+height:120px;
+}
+</style>
+</head>
+
+<body>
+
+<div id="box">
+<img src="photo.jpg">
+
+I am Chinmayi.<br>
+I am an MCA student.<br>
+I like programming.<br>
+I enjoy coding daily.<br>
+I love web development.<br>
+I like learning Java.<br>
+I practice HTML CSS.<br>
+I enjoy JavaScript.<br>
+I like problem solving.<br>
+My goal is software engineer.
+</div>
+
+</body>
+</html>
